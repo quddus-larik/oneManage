c3e691199ddc191021e6caf5ce80796605a113e8
quddus-larik <qudduslarek@proton.me>
2025-12-04 21:41:02 +0500

    feat: replace EmployeeTable with EmployeeManager for improved employee management

--- app/employees/page.tsx
+++ app/employees/page.tsx
@@ -1,16 +1,17 @@
-import { EmployeeTable } from "@/components/custom/employees-table";
+import { EmployeeManager } from "@/components/custom/employees";
 import { TitleHeader } from "@/components/custom/main-heading";
 import Dashboard from "@/app/provider/ui";
 
 export default function Page() {
-
-    return (
-        
-          <Dashboard>
-        <div className="flex gap-4 flex-col p-4 lg:p-6">
-            <TitleHeader label="Manage Employees" span="Control employees with detailed information." />
-            <EmployeeTable />
-        </div>
-          </Dashboard>
-    )
+  return (
+    <Dashboard>
+      <div className="flex gap-4 flex-col p-4 lg:p-6">
+        <TitleHeader
+          label="Manage Employees"
+          span="Control employees with detailed information."
+        />
+        <EmployeeManager />
+      </div>
+    </Dashboard>
+  );
 }
